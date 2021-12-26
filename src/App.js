aac923260692af2beec03dd00374adcdac221a3d
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-12-26 17:02:45 +0100

    Add top menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SelectView from "./views/select-view";
 import QuizView from "./views/quiz-view";
+import { AppBar, Container, Button } from "@mui/material";
+
+const views = [
+    { name: 'Verbs', route: '/' },
+    { name: 'Quiz', route: '/quiz' }
+];
 
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SelectView />} />
-          <Route path="/quiz" element ={<QuizView/>} />
-        </Routes>
+          <AppBar position="static">
+              <Container sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  { views.map(({ name, route }) => (
+                      <Link key={route} to={route}>
+                          <Button role="link" sx={{ my: 2, color: 'white'}}>{name}</Button>
+                      </Link>
+                  ))}
+              </Container>
+          </AppBar>
+            <Routes>
+              <Route path="/" element={<SelectView />} />
+              <Route path="/quiz" element ={<QuizView/>} />
+            </Routes>
+
       </BrowserRouter>
   );
 }
