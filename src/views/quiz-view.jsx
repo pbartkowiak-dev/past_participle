b715beba06aa94c2bbde09bf3d85c450f6395714
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-12-26 17:27:04 +0100

    Add quiz view

--- src/views/quiz-view.jsx
+++ src/views/quiz-view.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
-import { Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, TextField, Box, styled } from '@mui/material';
 
-function QuizView(props) {
+const Input = styled(TextField)(() => ({
+	minWidth: '360px',
+	'& .MuiTextField-root': {
+		fontSize: 43
+	}
+}));
+
+function QuizView() {
+	const [inputValue, setInputValue] = useState();
+	const [currentVerb, setCurrentVerb] = useState({});
+	const [showTranslation, setShowTranslation] = useState(false);
+
+	useEffect(() => {
+		setCurrentVerb({
+			infinitiv: "werden",
+			imperfekt: "wurde",
+			partizip_ii: "geworden",
+			translation_pol: "stawać się, zostawać",
+		})
+	});
+
 	return (
-		<Container></Container>
+		<Container sx={{textAlign: 'center', marginTop: 10 }}>
+			<Typography variant={"h3"}>{currentVerb.infinitiv}</Typography>
+			<Box sx={{height: '35px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+				{showTranslation
+					? <Typography variant={"h5"}>{currentVerb.translation_pol}</Typography>
+					: <Typography variant={"h5"}
+								  role="button"
+								  sx={{
+									  display: 'inline-block',
+									  color: 'lightgray',
+									  cursor: 'pointer',
+									  marginTop: 1
+								  }}
+								  onClick={() => setShowTranslation(true)}>Click to show translation</Typography>
+				}
+			</Box>
+			<Box sx={{ marginTop: 2 }}>
+				<Input id="outlined-basic" label="Enter Past Participle Form" variant="outlined" value={inputValue} />
+			</Box>
+		</Container>
 	);
 }
 
